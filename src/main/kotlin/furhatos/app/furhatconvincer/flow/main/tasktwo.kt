d2adfcf40722ac176ead6c0fd0a7dae622939618
Hujon99 <77735214+Hujon99@users.noreply.github.com>
2022-12-10 17:43:16 +0100

    Updated task two with gestures
    And some other stuff, but mainly gestures

--- src/main/kotlin/furhatos/app/furhatconvincer/flow/main/tasktwo.kt
+++ src/main/kotlin/furhatos/app/furhatconvincer/flow/main/tasktwo.kt
@@ -1,12 +1,16 @@
 package furhatos.app.furhatconvincer.flow.main
 
 import furhatos.app.furhatconvincer.flow.parents.Parent
+import furhatos.app.furhatconvincer.userData
+import furhat.libraries.standard.GesturesLib
 import furhatos.flow.kotlin.*
+import furhatos.gestures.Gestures
 import furhatos.nlu.SimpleIntent
 import furhatos.nlu.common.No
 import furhatos.nlu.common.Yes
 
-val completedTask = Button("Task completed!")
+val completedTask1 = Button("Task completed, 1 ticket!")
+val completedTask2 = Button("Task completed, 2 tickets!")
 val taskNotCompleted = Button("Task NOT completed")
 
 val okIntent = SimpleIntent("Okey", "Ok", "Okey, I can do it!", "Okey, I'll do it!", "Well, I'll do it!")
@@ -36,7 +40,10 @@ val TaskTwo : State = state(Parent) {
     }
 
     onResponse<No>{
-        furhat.say("Come on, it is going to be fun, it is not that embarrassing, I am here to keep you company")
+        furhat.say {
+            + "Come on, it is going to be fun, it is not that embarrassing, I am here to keep you company"
+            + Gestures.Roll
+        }
         goto(persuationPhase)
     }
 
@@ -51,34 +58,56 @@ val TaskTwo : State = state(Parent) {
 
 val tryingTask : State = state(Parent) {
     onEntry {
-        furhat.say("Awesome, I knew you would do it! Come on, just start whenever!")
+        furhat.say {
+            + "Awesome, I knew you would do it! Come on, just start whenever!"
+            + GesturesLib.PerformBigSmile1
+        }
     }
 
-    onButton(completedTask){
-        furhat.say("Hahaha great, you sounded like a real dog! Now let us move on to the final task")
+    onButton(completedTask1){
+        furhat.say {
+            + "Hahaha great, you sounded like a real dog! Now let us move on to the final task"
+            + GesturesLib.PerformBigSmile1
+        }
+        users.current.userData.tickets ++
+        goto(TaskThree)
+    }
+
+    onButton(completedTask2){
+        furhat.say {
+            + "Hahaha great, you sounded like a real dog! Now let us move on to the final task"
+            + GesturesLib.PerformBigSmile1
+        }
+        users.current.userData.tickets ++
+        users.current.userData.tickets ++
         goto(TaskThree)
     }
 
     onButton(taskNotCompleted) {
-        furhat.say("Well, what a shame! But I understand that it can be embarrasing, lets move on to the final task instead!")
-        furhat.say("I know you will love this one!")
+        furhat.say {
+            + "Well, what a shame! But I understand that it can be embarrasing, lets move on to the final task instead!"
+            + behavior { furhat.gesture(Gestures.Nod) }
+            + GesturesLib.PerformThoughtful2
+            + delay(500)
+            + "I know you will love this one!"
+            + GesturesLib.PerformSmile1
+        }
         goto(TaskThree)
     }
 }
 
 val persuationPhase : State = state(Parent) {
     onEntry {
-        furhat.ask("I promise, it will be worth it! Are you sure you don't want to try it?")
+        furhat.ask("I promise, it will be worth it! Do you want to try it?")
     }
 
     onReentry {
-        furhat.ask("So are you sure? You want to continue to the next task?")
+        furhat.ask("So are you sure? Do you want to bark?")
     }
 
     onResponse<Yes> {
-        furhat.say("Well, what a shame! But I understand that it can be embarrasing, lets move on to the final task instead!")
-        furhat.say("I know you will love this one!")
-        goto(TaskThree)
+        furhat.say("Okay, I'll take that as a YES, I wan't to do the task! Leets go!")
+        goto(tryingTask)
     }
 
     onResponse(okIntent) {
@@ -86,8 +115,9 @@ val persuationPhase : State = state(Parent) {
     }
 
     onResponse<No> {
-        furhat.say("Okay, I'll take that as a YES, I wan't to do the task! Leets go!")
-        goto(tryingTask)
+        furhat.say("Well, what a shame! But I understand that it can be embarrasing, lets move on to the final task instead!")
+        furhat.say("I know you will love this one!")
+        goto(TaskThree)
     }
 
     onNoResponse {
