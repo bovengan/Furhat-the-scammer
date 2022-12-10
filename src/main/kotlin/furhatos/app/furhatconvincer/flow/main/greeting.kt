eeb31662c3f2c5705d0d22fdec23bd0e26069643
bovengan <93683599+bovengan@users.noreply.github.com>
2022-12-10 13:35:14 +0100

    Update greeting.kt

--- src/main/kotlin/furhatos/app/furhatconvincer/flow/main/greeting.kt
+++ src/main/kotlin/furhatos/app/furhatconvincer/flow/main/greeting.kt
@@ -3,15 +3,21 @@ package furhatos.app.furhatconvincer.flow.main
 import furhatos.app.furhatconvincer.flow.Parent
 import furhatos.flow.kotlin.*
 import furhatos.nlu.common.Greeting
-import furhatos.nlu.common.No
-import furhatos.nlu.common.Yes
 
 val Greeting : State = state(Parent) {
     onEntry {
-        furhat.ask("Well hello there")
-        goto(Interview)
+        random(
+            {   furhat.ask("Hi there") },
+            {   furhat.ask("Hello there") }
+        )
     }
     onResponse<Greeting> {
-        goto(Interview)
+        goto(AskName)
+    }
+    onResponseFailed {
+        goto(AskName)
+    }
+    onNoResponse {
+        goto(AskName)
     }
 }
