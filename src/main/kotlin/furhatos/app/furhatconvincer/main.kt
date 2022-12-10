1ff5634dd8e7a12bfae232539bb8b139dc4399a1
Hujon99 <77735214+Hujon99@users.noreply.github.com>
2022-12-11 00:57:46 +0100

    Added global trigger for smile recognition

--- src/main/kotlin/furhatos/app/furhatconvincer/main.kt
+++ src/main/kotlin/furhatos/app/furhatconvincer/main.kt
@@ -1,11 +1,17 @@
 package furhatos.app.furhatconvincer
 
+//import furhatos.app.furhatconvincer.flow.*
+//import furhatos.skills.Skill
+//import furhatos.flow.kotlin.*
+
 import furhatos.app.furhatconvincer.flow.main.Idle
 import furhatos.app.furhatconvincer.setting.distanceToEngage
 import furhatos.app.furhatconvincer.setting.maxNumberOfUsers
+import furhatos.skills.Skill
 import furhatos.flow.kotlin.*
 import furhatos.flow.kotlin.voice.Voice
-import furhatos.skills.Skill
+import furhatos.gestures.Gestures
+import furhatos.skills.emotions.UserGestures
 
 class FurhatconvincerSkill : Skill() {
     override fun start() {
@@ -17,12 +23,17 @@ fun main(args: Array<String>) {
     Skill.main(args)
 }
 
-val Init: State = state {
+val Init : State = state() {
     init {
         /** Set our default interaction parameters */
         users.setSimpleEngagementPolicy(distanceToEngage, maxNumberOfUsers)
         furhat.voice = Voice("Matthew")
         /** start the interaction */
+        // Vet inte om denna trigger ska vara här eller någon annanstans men gör iaf att furhat tar leenden i hänsyn
+        // Och ler tillbaka om användaren ler.
+        onUserGesture(UserGestures.Smile) {
+            furhat.gesture(Gestures.BigSmile)
+        }
         goto(Idle)
     }
 }
