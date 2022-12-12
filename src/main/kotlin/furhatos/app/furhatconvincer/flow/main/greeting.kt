a40aed498b986ddf82318747517a24963dc9bea9
Hujon99 <77735214+Hujon99@users.noreply.github.com>
2022-12-12 16:08:24 +0100

    Updated greeting

--- src/main/kotlin/furhatos/app/furhatconvincer/flow/main/greeting.kt
+++ src/main/kotlin/furhatos/app/furhatconvincer/flow/main/greeting.kt
@@ -1,14 +1,19 @@
 package furhatos.app.furhatconvincer.flow.main
 
-import furhatos.app.furhatconvincer.flow.Parent
+import furhatos.app.furhatconvincer.flow.parents.Parent
 import furhatos.app.furhatconvincer.flow.main.interview.AskName
 import furhatos.flow.kotlin.*
+import furhatos.flow.kotlin.furhat.characters.Characters
+import furhatos.flow.kotlin.voice.Voice
 import furhatos.gestures.Gestures
 import furhatos.nlu.common.Greeting
 
 val Greeting: State = state(Parent) {
     onEntry {
-        furhat.gesture(Gestures.Smile)
+        furhat.setCharacter(Characters.Adult.Alex)
+        furhat.ledStrip.solid(java.awt.Color(0,0,0))
+        furhat.voice = Voice("Matthew")
+        furhat.gesture(Gestures.Smile(duration = 2.0))
         random(
             { furhat.ask("Hi there") },
             { furhat.ask("Hello there") }
