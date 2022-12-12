f2c949cf79bace8c958e46a7d0f260497121c002
Hujon99 <77735214+Hujon99@users.noreply.github.com>
2022-12-12 16:12:11 +0100

    Added second parent class

--- src/main/kotlin/furhatos/app/furhatconvincer/flow/parents/parent.kt
+++ src/main/kotlin/furhatos/app/furhatconvincer/flow/parents/parent.kt
@@ -1,7 +1,9 @@
-package furhatos.app.furhatconvincer.flow
+package furhatos.app.furhatconvincer.flow.parents
 
 import furhatos.app.furhatconvincer.flow.main.Idle
 import furhatos.flow.kotlin.*
+import furhatos.gestures.Gestures
+import furhatos.skills.emotions.UserGestures
 
 val Parent: State = state {
 
@@ -15,8 +17,21 @@ val Parent: State = state {
     onUserEnter(instant = true) {
         furhat.glance(it)
     }
-    
+
     onUserGesture(instant = true, gesture = UserGestures.Smile) {
-        furhat.gesture(Gestures.BigSmile)
+        furhat.gesture(Gestures.BigSmile(duration = 1.5))
+    }
+}
+
+val Parent2: State = state {
+
+    onUserEnter(instant = true) {
+        furhat.glance(it)
+
+        furhat.attend(it)
+    }
+
+    onUserGesture(instant = true, gesture = UserGestures.Smile) {
+        furhat.gesture(Gestures.BigSmile(duration = 1.5))
     }
 }
