dd24e991de662c7f4d3d2b52747947ae4dc196d8
Hujon99 <77735214+Hujon99@users.noreply.github.com>
2022-12-16 07:33:55 +0100

    Update askname.kt

--- src/main/kotlin/furhatos/app/furhatconvincer/flow/main/interview/askname.kt
+++ src/main/kotlin/furhatos/app/furhatconvincer/flow/main/interview/askname.kt
@@ -1,12 +1,15 @@
 package furhatos.app.furhatconvincer.flow.main.interview
 
 import furhatos.app.furhatconvincer.flow.main.TaskThree
+import furhatos.app.furhatconvincer.flow.main.userSaidWhy
 import furhatos.app.furhatconvincer.flow.parents.Parent
 import furhatos.app.furhatconvincer.nlu.JustName
+import furhatos.app.furhatconvincer.nlu.whyIntent
 import furhatos.app.furhatconvincer.userData
 import furhatos.flow.kotlin.*
 import furhatos.gestures.Gestures
 import furhatos.nlu.common.TellName
+
 
 val AskName: State = state(Parent) {
     onEntry {
@@ -15,13 +18,18 @@ val AskName: State = state(Parent) {
     }
 
     onReentry {
-        users.current.userData.askNameReentry ++
-        furhat.gesture(Gestures.Oh(duration = 1.5))
-        random(
-            { furhat.ask("I am sorry, I did not quite understand what you said, what is your name?") },
-            { furhat.ask("Oh sorry, I did not quite catch that, can you repeat your name?") },
-            { furhat.ask("Sorry, can you say your name one more time?") }
-        )
+        if (userSaidWhy){
+            furhat.ask("So what is your name?")
+            userSaidWhy = false
+        }else {
+            users.current.userData.askNameReentry++
+            furhat.gesture(Gestures.Oh(duration = 1.5))
+            random(
+                { furhat.ask("I am sorry, I did not quite understand what you said, what is your name?") },
+                { furhat.ask("Oh sorry, I did not quite catch that, can you repeat your name?") },
+                { furhat.ask("Sorry, can you say your name one more time?") }
+            )
+        }
     }
 
     onResponse<TellName> {
@@ -52,6 +60,12 @@ val AskName: State = state(Parent) {
             { furhat.say("It is a pleasure meeting you " + users.current.userData.name) }
         )
         goto(AskAge)
+    }
+
+    onResponse(whyIntent) {
+        furhat.say("I mean, having a name creates a more personal relation!")
+        userSaidWhy = true
+        reentry()
     }
 
 
